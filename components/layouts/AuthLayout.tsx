a13b02b56f0e139c4d7f3acbe16de99f47e40427
Kubeworkz <kubeworkz@gmail.com>
2023-06-02 17:01:42 -0400

    add logo links

--- components/layouts/AuthLayout.tsx
+++ components/layouts/AuthLayout.tsx
@@ -9,11 +9,13 @@ export default function AuthLayout({ children, heading, description }: Props) {
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div>
-          <img
-            src="https://lh3.googleusercontent.com/drive-viewer/AFGJ81okqmUGTG9YOoa34NsZd4dG0MXoh805rcLZs4SMgqA6p0tI9dVkxlwQp5aljcZCHwtwAUOltSHr3OyL3UTmnZy3-_0UfQ=s1600"
-            className="mx-auto" style={{width: "auto", height: "140px"}}
-            alt="SaasHQ"
-          />
+          <a href="https://saashq.org">
+            <img
+              src="https://lh3.googleusercontent.com/drive-viewer/AFGJ81okqmUGTG9YOoa34NsZd4dG0MXoh805rcLZs4SMgqA6p0tI9dVkxlwQp5aljcZCHwtwAUOltSHr3OyL3UTmnZy3-_0UfQ=s1600"
+              className="mx-auto" style={{width: "auto", height: "140px"}}
+              alt="SaasHQ"
+            />
+          </a>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             {heading}
           </h2>
