8435bd08fd75414bbcc355634f38f8a9b9c3db04
Kubeworkz <kubeworkz@gmail.com>
2023-06-02 13:48:31 -0400

    add logo to hero

--- components/ui/landing/HeroSection.tsx
+++ components/ui/landing/HeroSection.tsx
@@ -7,6 +7,11 @@ const HeroSection = () => {
     <div className="hero py-52">
       <div className="hero-content text-center">
         <div className="max-w-7md">
+          <img
+            src="https://lh3.googleusercontent.com/drive-viewer/AFGJ81okqmUGTG9YOoa34NsZd4dG0MXoh805rcLZs4SMgqA6p0tI9dVkxlwQp5aljcZCHwtwAUOltSHr3OyL3UTmnZy3-_0UfQ=s1600"
+            className="mx-auto" style={{width: "auto", height: "140px"}}
+            alt="SaasHQ"
+          />
           <h1 className="text-5xl font-bold"> {t('enterprise-saas-kit')}</h1>
           <p className="py-6 text-2xl font-normal">
             {t('kickstart-your-enterprise')}
