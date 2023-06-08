5891f3cabdd3dbcbd1aea85eb818d7581e067adf
Kubeworkz <kubeworkz@gmail.com>
2023-06-08 15:37:20 -0400

    prep for admin dashboard

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,4 +1,5 @@
 // eslint-disable-next-line no-use-before-define
+import { Product, Role } from '@prisma/client';
 import NextAuth, { DefaultSession } from 'next-auth';
 
 declare module 'next-auth' {
@@ -6,8 +7,16 @@ declare module 'next-auth' {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user: {
+    user?: {
       id: string;
+      role: Role;
+      image: string;
+      email: string;
+      name: string;
+      isAdmin: boolean;
+      product: Product | null;
+      stripeCustomerId: string;
+      onboarded: boolean;
     } & DefaultSession['user'];
   }
 }
