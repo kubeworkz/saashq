b6b0360aad9ab89f414b45f6c41fdf6363ab9d84
Kubeworkz <kubeworkz@gmail.com>
2023-06-21 12:43:53 -0400

    add badge component

--- components/shared/Badge.tsx
+++ components/shared/Badge.tsx
@@ -1,31 +1,18 @@
-import classNames from "classnames";
+import { BadgeProps, Badge as BaseBadge } from 'react-daisyui';
 
-export function Badge(props: {
-  size?: string;
-  variant?: string;
-  children: any;
-}) {
+const Badge = (props: BadgeProps) => {
+  const { children, className } = props;
+
   return (
-    <div
-      className={classNames(
-        "flex gap-1 rounded-lg px-[.5rem] py-[.2rem] ",
-        { "text-sm": props.size == "sm" || !props.size },
-        { "text-base": props.size == "md" },
-        { "text-lg": props.size == "lg" },
-        { "text-xs": props.size == "xs" },
-        {
-          "bg-neutral-600/20 text-neutral-600":
-            props.variant == "default" || !props.variant,
-        },
-        { "bg-emerald-600/20 text-emerald-600": props.variant == "success" },
-        { "bg-amber-600/20 text-amber-600": props.variant == "warning" },
-        { "bg-slate-100 text-black": props.variant == "info" },
-        { "bg-rose-600/20 text-rose-600": props.variant == "error" },
-      )}
-    >
-      {props.children}
-    </div>
+    <>
+      <BaseBadge
+        {...props}
+        className={`rounded text-xs py-2 text-white ${className}`}
+      >
+        {children}
+      </BaseBadge>
+    </>
   );
-}
+};
 
 export default Badge;
