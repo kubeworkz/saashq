e19dd2996bbeb026d5109cf97cec54e8340cbd1b
Kubeworkz <kubeworkz@gmail.com>
2023-07-04 14:17:24 -0400

    Add team switcher to the sidebar

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,15 +2,25 @@ import { CreateProject, Projects } from '@/components/project';
 import { GetServerSidePropsContext } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { Button } from 'react-daisyui';
 import type { NextPageWithLayout } from 'types';
 
 const AllProjects: NextPageWithLayout = () => {
   const [visible, setVisible] = useState(false);
 
+  const router = useRouter();
   const { t } = useTranslation('common');
 
+  const { newProject } = router.query as { newProject: string };
+
+  useEffect(() => {
+    if (newProject) {
+      setVisible(true);
+    }
+  }, [router.query]);
+  
   return (
     <>
       <div className="flex items-center justify-between">
