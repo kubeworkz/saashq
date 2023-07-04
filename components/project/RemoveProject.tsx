f08072c291b5e029aaed23666f001fe16ad62f40
Kubeworkz <kubeworkz@gmail.com>
2023-07-04 12:25:03 -0400

    Add confirmation before leaving or removing project

--- components/project/RemoveProject.tsx
+++ components/project/RemoveProject.tsx
@@ -3,60 +3,65 @@ import { Project } from '@prisma/client';
 import axios from 'axios';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-daisyui';
 import toast from 'react-hot-toast';
+
+import ConfirmationDialog from '../shared/ConfirmationDialog';
 
 const RemoveProject = ({ project }: { project: Project }) => {
   const router = useRouter();
   const { t } = useTranslation('common');
-  const [loading, setLoading] = React.useState(false);
+  const [loading, setLoading] = useState(false);
+  const [askConfirmation, setAskConfirmation] = useState(false);
 
   const removeProject = async () => {
-    const confirm = window.confirm(
-      'Are you sure you want to remove this project? This action cannot be undone.'
-    );
+    setLoading(true);
 
-    if (!confirm) return;
+    const response = await axios.delete(`/api/projects/${project.slug}`);
 
-    try {
-      setLoading(true);
-      const response = await axios.delete(`/api/projects/${project.slug}`);
-      setLoading(false);
+    setLoading(false);
 
-      const { data, error } = response.data;
+    const { data, error } = response.data;
 
-      if (error) {
-          toast.error(error.message);
-          return;
-      }      
+    if (error) {
+      toast.error(error.message);
+      return;
+    }
 
-      if (data) {
-        toast.success('Project removed successfully!');
-        return router.push('/projects');
-      } 
-    } catch(error: any) {
-      toast.error(`Project will not delete as Database ${project.slug} doesn't exists.`);
+    if (data) {
+      toast.success(t('project-removed-successfully'));
+      return router.push('/projects');
     }
   };
 
   return (
-    <Card heading="Remove Project">
-      <Card.Body className="px-3 py-3">
-        <div className="space-y-3">
-          <p className="text-sm">{t('remove-project-warning')}</p>
-          <Button
-            color="error"
-            size="sm"
-            onClick={removeProject}
-            loading={loading}
-            className="text-white"
-          >
-            {t('remove-project')}
-          </Button>
-        </div>
-      </Card.Body>
-    </Card>
+    <>
+      <Card heading={t('remove-project')}>
+        <Card.Body className="px-3 py-3">
+          <div className="flex flex-row gap-4">
+            <p className="text-sm">{t('remove-project-warning')}</p>
+            <Button
+              color="error"
+              onClick={() => setAskConfirmation(true)}
+              loading={loading}
+              variant="outline"
+              size="sm"
+            >
+              {t('remove-project')}
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+      <ConfirmationDialog
+        visible={askConfirmation}
+        title={t('remove-project')}
+        onCancel={() => setAskConfirmation(false)}
+        onConfirm={removeProject}
+      >
+        {t('remove-project-confirmation')}
+      </ConfirmationDialog>
+    </>
   );
 };
 
